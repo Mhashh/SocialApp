3c5695faf53acf3e95617532ae2de99417731e54
“Mhashh” <m04tiria@outlook.com>
2022-06-02 21:26:47 +0530

    signin added

--- server/endfunctions/authentication.js
+++ server/endfunctions/authentication.js
@@ -1,7 +1,10 @@
 const user = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 exports.signup = function(req,res){
+    //destructure response body
     const {username,email,password} = req.body;
+    //check for email existing
     user.findOne({email}).exec(function(err,user){
         if (user) {
             return res.status(400).json({
@@ -9,6 +12,7 @@ exports.signup = function(req,res){
             });
     }})
 
+    //check for username existing
     user.findOne({username}).exec(function(err,user){
         if (user) {
             return res.status(400).json({
@@ -16,7 +20,10 @@ exports.signup = function(req,res){
             });
     }})
 
+    //new details to save in db
     const newuser = new user({username,email,password})
+    
+    //save in db
     newuser.save((err,newuser)=>{
         if(err){
             console.log('SAVED USER IN ACCOUNT ACTIVATION')
@@ -30,4 +37,38 @@ exports.signup = function(req,res){
         })
             
     })
+}
+
+exports.signin = function(req,res){
+    const {email,password} = req.body;
+    //find target account
+    user.findOne({email}).exec(function(err,olduser){
+        //error or unknown email
+        if (err || !olduser) {
+            return res.status(400).json({
+              error: "No account with given email exists!",
+        })};
+        
+        //check if password correct
+        if(!olduser.authenticate(password)){
+            return res.json({
+                error:'Email and password do not match.'
+            })
+        }
+        
+        //creating a jwt token
+        const token = jwt.sign({id:olduser.id},process.env.JWT_SECRET,{expiresIn:'4d'});
+        const username = olduser.username;
+        const id = olduser.id
+        //sending username and detail
+        return res.json({
+            token,
+            user:{id,username,email}
+        })
+
+    })
+
+    
+
+
 }
--- server/routes/authenticate.js
+++ server/routes/authenticate.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {signup} = require('../endfunctions/authentication')
+const {signup,signin} = require('../endfunctions/authentication')
 router.post('/signup',signup);
-router.post('/signin');
+router.post('/signin',signin);
 
 module.exports = router;
