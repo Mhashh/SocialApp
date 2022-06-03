4844e8ab3725365d159662ccaabb7a4fc7b3fd43
“Mhashh” <m04tiria@outlook.com>
2022-06-03 23:54:56 +0530

    link based acc activation added

--- server/endfunctions/authentication.js
+++ server/endfunctions/authentication.js
@@ -21,9 +21,10 @@ exports.signup = function(req,res){
             });
     }})
     
-    //new details to save in db
-    const newuser = new user({username,email,password})
+    //creating a jwt token
+    const token = jwt.sign({username,email,password},process.env.JWT_AUTH_SECRET,{expiresIn:'15m'});
     
+    //sending activation link
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -39,32 +40,75 @@ exports.signup = function(req,res){
       let mailOptions = {
         from: "054maheshtiria@gmail.com",
         to: `${email}`,
-        subject: 'Chatcafe account activation',
-        text: 'Hi from your chatcafe project'
+        subject: 'Moretalk account activation',
+        html: `
+                <h1>Please use the following link to activate your account</h1>
+                <p>${process.env.AUTH_LINK}${token}</p>
+                <hr />
+                <p>This email may contain sensitive information</p>
+            `,
       };
 
       transporter.sendMail(mailOptions, function(err, data) {
         if (err) {
           console.log("Error " + err);
+          return res.status(401).json({
+            error:"unable to send actiavtion mail, Pls try again later"
+          })
+          
         } else {
-          console.log("Email sent successfully");
+          console.log(`Email sent successfully ${token}`);
+          return res.json({
+            message:"An activation mail has been sent. Pls click the link in mail to activate the account."
+          })
+
         }
       });
 
-    //save in db
-    newuser.save((err,newuser)=>{
-        if(err){
-            console.log('SAVED USER IN ACCOUNT ACTIVATION')
-            return res.status(401).json({
-                error:'ERROR saving user in database'
-            })
-        }
+    
+}
 
-        return res.json({
-        message:'Signup success.PLease signin'
-        })
+exports.accountActivation = function(req,res){
+  //new details to save in db
+  const {token} = req.body;
+
+    if(token){
+        jwt.verify(token,process.env.JWT_AUTH_SECRET,function(err,decoded){
+            if(err){
+                console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR')
+                return res.status(401).json({
+                    error:'Expired link. SIgnup again'
+                })
+            }
+            console.log(decoded);
+            const {username,email,password} = jwt.decode(token)
+            const newuser = new user({username,email,password});
+
+            //save in db
+            newuser.save((err,newuser)=>{
+              if(err){
+                  console.log('SAVED USER IN ACCOUNT ACTIVATION')
+                  return res.status(401).json({
+                      error:'ERROR saving user in database'
+                  })
+              }
+
+              return res.json({
+              message:'Signup success.PLease signin'
+              })
+                  
+          })
             
-    })
+        })
+                
+    }
+
+    else{
+        return res.json({
+            message:'Something went wrong. Try again'
+        })
+    }
+  
 }
 
 exports.signin = function(req,res){
--- server/routes/authenticate.js
+++ server/routes/authenticate.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {signup,signin} = require('../endfunctions/authentication')
+const {signup,signin,accountActivation} = require('../endfunctions/authentication')
 const {userSignupValidator,userSigninValidator} = require('../validators/authvalidation')
 router.post('/signup',userSignupValidator,signup);
 router.post('/signin',userSigninValidator,signin);
-
+router.post('/accountactivation/',accountActivation);
 module.exports = router;
