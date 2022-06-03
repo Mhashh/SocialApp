69ae9a24a1ec9d7c393870c96eac94446a15d3f3
“Mhashh” <m04tiria@outlook.com>
2022-06-04 00:48:32 +0530

    friendlist->colleaguelist

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const {signup,signin,accountActivation} = require('../endfunctions/authentication')
+router.post('/addpost',);
+router.post('/deletepost',);
+router.post('/addcolleague',accountActivation);
+module.exports = router;
