30f59f58c649b51c5d6489f1346e1c410a707b2c
“Mhashh” <m04tiria@outlook.com>
2022-07-07 17:49:55 +0530

    isSignedIn check

--- server/middlefunctions/usermiddleauth.js
+++ server/middlefunctions/usermiddleauth.js
@@ -1,5 +1,40 @@
 const expressjwt = require('express-jwt')
+const user = require('../models/user')
+const jwt = require('jsonwebtoken')
 exports.signin = expressjwt({
         secret:`${process.env.JWT_AUTH}`,
         algorithms:["HS256"]
     })
+
+exports.isSigninedIn = function(req,res,next){
+    const {token} = req.body
+    if(token){
+        jwt.verify(token, function(error,decoded){
+        if(error){
+            return res.status(400).json({
+            error:"Invalid user access."
+            })
+        }
+        else{
+            user.findOne(decoded).exec(
+            function(err,userid){
+                if(err){
+                return res.status(400).json({
+                    error:"Invalid user access."
+                })
+                }
+                else{
+                next()
+                }
+            }
+    
+    
+            )
+        }
+        })
+    }else{
+        return res.status(400).json({
+        error:"Invalid user access."
+        })
+    }
+    }
